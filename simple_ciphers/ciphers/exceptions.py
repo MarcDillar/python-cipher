9cf7328d79c395a514734388f893697913439212
mdillar <marc.dillar@gmail.com>
2021-07-18 01:02:39 +0200

    Fixed code formatting

--- simple_ciphers/ciphers/exceptions.py
+++ simple_ciphers/ciphers/exceptions.py
@@ -1,9 +1,11 @@
 """ Cipher Exception classes """
 
+
 class IncorrectCipherKeyError(ValueError):
     """Error raised if the cipher key is incorrect"""
     def __init__(self, message=""):
         super().__init__(message)
+
 
 class IncorrectMessageError(ValueError):
     """Error raised if the message to encrypt/decrypt is incorrect"""
