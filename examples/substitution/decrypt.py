9e1ffe072e339ff419b22e21ef318c4619a5ca48
mdillar <marc.dillar@gmail.com>
2021-07-19 02:19:25 +0200

    Add examples with mapping dictionaries

--- examples/substitution/decrypt.py
+++ examples/substitution/decrypt.py
@@ -3,9 +3,43 @@
 from simple_ciphers.ciphers.substitution import SimpleSubstitutionCipher
 
 MESSAGE_TO_DECRYPT = "Iaca sr jia narrlua jilj xaaor jp fa axwchbjao"
-KEY = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
 
 cipher = SimpleSubstitutionCipher()
 
+# Decryption with a key
+KEY = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
 decrypted_message = cipher.decrypt(message=MESSAGE_TO_DECRYPT, key=KEY)
+print(decrypted_message)
+
+# Decryption with a mapping dictionnary
+MAPPING = {
+        "A": "L",
+        "B": "F",
+        "C": "W",
+        "D": "O",
+        "E": "A",
+        "F": "Y",
+        "G": "U",
+        "H": "I",
+        "I": "S",
+        "J": "V",
+        "K": "K",
+        "L": "M",
+        "M": "N",
+        "N": "X",
+        "O": "P",
+        "P": "B",
+        "Q": "D",
+        "R": "C",
+        "S": "R",
+        "T": "J",
+        "U": "T",
+        "V": "Q",
+        "W": "E",
+        "X": "G",
+        "Y": "H",
+        "Z": "Z"
+}
+
+decrypted_message = cipher.decrypt(message=MESSAGE_TO_DECRYPT, mapping=MAPPING)
 print(decrypted_message)
--- examples/substitution/encrypt.py
+++ examples/substitution/encrypt.py
@@ -3,9 +3,42 @@
 from simple_ciphers.ciphers.substitution import SimpleSubstitutionCipher
 
 MESSAGE_TO_ENCRYPT = "Here is the message that needs to be encrypted"
-KEY = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
-
 cipher = SimpleSubstitutionCipher()
 
+# Encryption with a key
+KEY = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
 encrypted_message = cipher.encrypt(message=MESSAGE_TO_ENCRYPT, key=KEY)
+print(encrypted_message)
+
+# Encryption with a mapping dictionnary
+MAPPING = {
+        "A": "L",
+        "B": "F",
+        "C": "W",
+        "D": "O",
+        "E": "A",
+        "F": "Y",
+        "G": "U",
+        "H": "I",
+        "I": "S",
+        "J": "V",
+        "K": "K",
+        "L": "M",
+        "M": "N",
+        "N": "X",
+        "O": "P",
+        "P": "B",
+        "Q": "D",
+        "R": "C",
+        "S": "R",
+        "T": "J",
+        "U": "T",
+        "V": "Q",
+        "W": "E",
+        "X": "G",
+        "Y": "H",
+        "Z": "Z"
+}
+
+encrypted_message = cipher.encrypt(message=MESSAGE_TO_ENCRYPT, mapping=MAPPING)
 print(encrypted_message)
