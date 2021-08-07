f3f8d596e489bb21eb449cc53f035cb34917711d
Marc Dillar <marc.dillar@gmail.com>
2021-08-08 00:57:27 +0200

    feature(hacker): move util methods to the Hacker parent class

--- simple_ciphers/hackers/hacker.py
+++ simple_ciphers/hackers/hacker.py
@@ -28,3 +28,36 @@ class Hacker:
             model,
             norm_probs=True
         )
+
+    def _candidate(self, message, p):
+        lang, prob = self.lang_identifier.classify(message)
+        if lang == self.language and prob >= p:
+            return True, {
+                "text": message,
+                "p": prob
+            }
+        return False, None
+
+    def _get_candidate(self, cipher, p, **kwargs):
+        decrypted_message = cipher.decrypt(**kwargs)
+
+        is_candidate, candidate = self._candidate(decrypted_message, p)
+
+        if is_candidate:
+            return candidate
+
+    @staticmethod
+    def _clean_list(decrypted_messages):
+        decrypted_messages = sorted(
+            decrypted_messages,
+            key=lambda x: x["p"],
+            reverse=True
+        )
+
+        final_list = []
+        for message in decrypted_messages:
+            text = message["text"]
+            if text not in final_list:
+                final_list.append(text)
+
+        return final_list
