f3a9ef1844cb9828d647b18570d96984b82a83b7
Marc Dillar <marc.dillar@gmail.com>
2021-08-26 23:06:36 +0200

    feature: added is_prime util method

--- simple_ciphers/utils/math.py
+++ simple_ciphers/utils/math.py
@@ -87,3 +87,30 @@ def prime_eratosthenes(n):
             for j in range(i*i, n+1, i):
                 prime_list.append(j)
     return prime_list
+
+
+def is_prime(n):
+    """
+    Return True if the integer parameter is prime
+    """
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    r = int(n**0.5)
+    # since all primes > 3 are of the form 6n ± 1
+    # start with f=5 (which is prime)
+    # and test f, f+2 for being prime
+    # then loop by 6.
+    f = 5
+    while f <= r:
+        if n % f == 0:
+            return False
+        if n % (f+2) == 0:
+            return False
+        f += 6
+    return True
